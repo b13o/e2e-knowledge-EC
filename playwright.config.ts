b99592fd02f71c32385d5960086b6974e0add9ed
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2025-03-16 17:46:19 +0900

    認証フローのE2Eテストを実装

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+
+  globalSetup: "./e2e/config/globalSetup.ts",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,6 +32,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    storageState: "./e2e/config/storageState.json",
   },
 
   /* Configure projects for major browsers */
